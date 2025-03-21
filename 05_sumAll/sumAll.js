476ddec40023a3343cee77b1dc3ac7e4cd8428b8
Anneman19 <199607389+Anneman19@users.noreply.github.com>
2025-03-21 20:19:11 +0200

    Change stuff to work

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let [first, second] = args;
 
+    if (first < 0 || second < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(first) || !Number.isInteger(second)) {
+        return "ERROR";
+    } else if (Number.isNaN(first) || Number.isNaN(second)) {
+        return "ERROR";
+    } else {
+        let sum = 0;
+        let largeNum = 0;
+        let smallNum = 0;
+        
+        if (first >= second) {
+            largeNum = first;
+            smallNum = second;
+        } else if (second > first) {
+            largeNum = second;
+            smallNum = first;
+        }
+        for (let i = smallNum; i <= largeNum; i++) {
+            sum += i;
+            }
+        return sum;    
+    }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
