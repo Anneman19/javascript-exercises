ca4b4cb9cd8ee174ef46972ebe67dd5052642b01
Anneman19 <199607389+Anneman19@users.noreply.github.com>
2025-03-21 11:15:32 +0200

    Change stuff to work

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let letters = [];
 
+    for (let i = 0; i < string.length; i++) {
+        letters.push(string.charAt(i));
+    }
+
+    let reverseLetters = [];
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverseLetters.push(letters.at(i));
+    }
+    return reverseLetters.join("");
 };
-
 // Do not edit below this line
 module.exports = reverseString;
