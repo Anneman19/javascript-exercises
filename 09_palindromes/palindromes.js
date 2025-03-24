fa0537a6ed97b3568c446f9bf2c3edbf7bc06611
Anneman19 <199607389+Anneman19@users.noreply.github.com>
2025-03-25 00:40:33 +0200

    Change stuff to work

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+    let reverseString = processedString.split('').reverse().join('');
+
+    return processedString === reverseString;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
