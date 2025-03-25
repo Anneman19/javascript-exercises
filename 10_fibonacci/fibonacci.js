1dc8a59a2410a436f2079904957c1eb7820dc674
Anneman19 <199607389+Anneman19@users.noreply.github.com>
2025-03-25 21:29:27 +0200

    Change stuff to work

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let number = parseInt(num);
+    if (number < 0) {
+        return "OOPS";
+    } else if (number === 0) {
+        return 0;
+    }
+    let fibonacciArr = [1, 1];
+    for (let i = 2; i < number; i++) {
+        fibonacciArr.push(fibonacciArr[i - 1] + fibonacciArr[i - 2]);
+    }
+    return fibonacciArr.at(number - 1);
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
