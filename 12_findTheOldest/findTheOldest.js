fc8a68616fc522dd640c6e40a8ddbc0d0c7693be
Anneman19 <199607389+Anneman19@users.noreply.github.com>
2025-03-25 22:18:46 +0200

    Change stuff to work

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldestAge = -Infinity;
+    let oldestPerson = null;
 
+    array.forEach(person => {
+        const currentYear = new Date().getFullYear();
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = currentYear;
+        }
+        const age = person.yearOfDeath - person.yearOfBirth;
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
